1aff3397f11a20a3ad9c6263df360e36050f32f8
Alex <pratt.alex@ymail.com>
2014-05-01 00:10:23 -0400

    Added comments

--- plt.py
+++ plt.py
@@ -1,18 +1,25 @@
 pyg = 'ay'
 o_sent = raw_input("What would you like to translate? No symbols please")
+#User input
 l_osent = o_sent.lower()
-sym = ["!", "@", "#", "$", "%", "^", "&", "*", "(" , ")"]
+#Simplifies things
 n_word = []
 t_sent = []
+#empty lists for later
 n_word = l_osent.split(" ")
+#splits the string into a list
 for n in n_word:
 	if n.isalpha():
 		f_letter = n[0]
+        #takes the first letter
 		f_w = n[1:] + f_letter + pyg
+        #concatenates the word minus the first letter, with the first letter and ay
 		t_sent.append(f_w)
+        #adds it to the list
         else:
             print "All numbers must be spelled out."
-	
+            #in case someone inputs numbers
 
 f = " ".join(t_sent)
+#reforms the string
 print f
